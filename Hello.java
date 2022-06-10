b7c465d8c2e3d9b7d144272ad5d7a8db011c4068
Percyeva271098 <105909425+Percyeva271098@users.noreply.github.com>
2022-06-10 12:37:23 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,14 @@
+class Test{
+
+public static void main(String[] args){
+System.out.println("Calling Test");
+}
+}
+class Hello{
+
+public static void main(String[] args){
+System.out.println("Hello World");
+System.out.print("Welcome to the World of Java Programming\t");
+System.out.println("Language");
+}
+}
